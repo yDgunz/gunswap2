8cac5e2f0684ec605bd1c3c87698c3cf3d10226d
Eric Gunther <eric.gunther@Diannes-MacBook-Air.local>
2025-03-29 13:38:25 -0400

    setting up react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Juggling Simulator</h1>
+        <p>Coming soon: Interactive juggling patterns and animations!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
